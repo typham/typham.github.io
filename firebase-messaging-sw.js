9311fb581851d30848b3337b5b050e25a73c85eb
Phạm Tý <phamty113@gmail.com>
2016-11-09 17:39:46 +0700

    Upload Test firebase

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,41 @@
+// [START initialize_firebase_in_sw]
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB-26mupGFJbe-szXjBAgpOKAdcVcr_yYo",
+    authDomain: "syncdevice-7f6e2.firebaseapp.com",
+    databaseURL: "https://syncdevice-7f6e2.firebaseio.com",
+    storageBucket: "syncdevice-7f6e2.appspot.com",
+    messagingSenderId: "938046412598"
+};
+firebase.initializeApp(config);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+// [END initialize_firebase_in_sw]
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
+// [END background_handler]
