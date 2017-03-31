9e7ded0bb88786986cc12e7745095583f8455a3d
Tý Phạm <T Phm>
2017-03-31 21:28:32 +0700

    Empty rep

--- js/onlineState.js
+++
@@ -1,9 +0,0 @@
-var ref = database.ref("Users/typham");
-ref.update({
-   onlineState: true,
-   status: "I'm online."
-});
-ref.onDisconnect().update({
-  onlineState: false,
-  status: "I'm offline."
-});
